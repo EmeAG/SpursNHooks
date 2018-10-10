16e05b6a1ee23db919f45dc5cb9bd1e7171de3cd
dani020998 <43583536+dani020998@users.noreply.github.com>
2018-10-10 11:39:44 +0200

    Suelos colocados

--- Game/Battle.js
+++ Game/Battle.js
@@ -1,20 +1,89 @@
 Game.Battle = function(game){
-	var dinero_P1=1000;
-	var dinero_P2=1000;
-	
-	
-	
+	var SueloPirata =null;
+
+
 };
 
 
 Game.Battle.prototype ={
 	create:function(){
-		this.stage.backgroundColor = '#E05538';
-		
+		//imagen mala orientacion
+		this.image_turn =this.add.image(0, 0, "landscape");		
+		//Imagen Fondo
+        this.background = this.add.image(0, 0, "background");
+        this.background.height = this.game.height;
+        this.background.width = this.game.width;
+		//Suelos
+        this.SueloPirata=this.add.sprite(0, this.world.height- this.cache.getImage("Suelo_Pirata").height, 'Suelo_Pirata');
+	    this.SueloVaquero=this.add.sprite(this.world.width-this.world.width/3, this.world.height- this.cache.getImage("Suelo_Pirata").height, 'Suelo_Pirata');
+	    this.SueloMar=this.add.sprite(this.world.width-this.world.width/3*2, this.world.height- this.cache.getImage("Suelo_Pirata").height, 'Suelo_Pirata');
+
 	},
 	
-	update: function(){
-		
-	}
+	update:function(){
+		if (this.scale.isPortrait){
+			this.image_turn.height = this.game.height;
+			this.image_turn.width = this.game.width;
+			this.image_turn.visible=true;
+			this.image_turn.bringToTop();
+		}else{
+			if (this.image_turn.visible === true){
+				this.image_turn.visible=false;
+			}
+		}
+		this.resize();
+	},
 	
+
+
+
+	getSpriteScale: function (spriteWidth, spriteHeight, availableSpaceWidth, availableSpaceHeight, minPadding) {
+		var ratio = 1;
+		var currentDevicePixelRatio = window.devicePixelRatio;
+		// Sprite needs to fit in either width or height
+		
+		var widthRatio = (spriteWidth * currentDevicePixelRatio + 2 * minPadding) / availableSpaceWidth;
+		var heightRatio = (spriteHeight * currentDevicePixelRatio + 2 * minPadding) / availableSpaceHeight;
+		if(widthRatio > 1 || heightRatio > 1){
+			ratio = 1 / Math.max(widthRatio, heightRatio);
+		} 
+		return ratio * currentDevicePixelRatio;
+	},
+	scaleSprite: function (sprite, availableSpaceWidth, availableSpaceHeight, padding, scaleMultiplier) {
+		var scale = this.getSpriteScale(sprite.width, sprite.height, availableSpaceWidth, availableSpaceHeight, padding);
+		sprite.scale.x = scale * scaleMultiplier;
+		sprite.scale.y = scale * scaleMultiplier;
+	},
+
+	resize: function () {
+		this.porcentaje_logo_juego=30;//espacio de la pantalla que se reserva al titulo del juego
+		multiplicador_escala=0.75;//valor por el que se escalaran los botones, depende del tamaño de la pantalla
+
+		this.background.height = this.world.height;
+		this.background.width = this.world.width;
+		
+		this.SueloPirata.height = this.world.height/6;
+		this.SueloPirata.width = this.world.width/3;
+		this.SueloPirata.x=0;
+		this.SueloPirata.y=this.world.height-this.SueloPirata.height;
+		
+		this.SueloVaquero.height = this.world.height/6;
+		this.SueloVaquero.width = this.world.width/3;
+		this.SueloVaquero.x=this.world.width-this.world.width/3;
+		this.SueloVaquero.y=this.world.height-this.SueloVaquero.height;
+		
+		this.SueloMar.height = this.world.height/6;
+		this.SueloMar.width = this.world.width/3;
+		this.SueloMar.x=this.world.width-this.world.width/3*2;
+		this.SueloMar.y=this.world.height-this.SueloMar.height;	
+		
+	},
+
+
+	
+	
+	
+	render:function() {
+		this.game.debug.text(this.SueloPirata.height ,40,50,"white");
+	}
 };
--- Game/MainMenu.js
+++ Game/MainMenu.js
@@ -3,13 +3,25 @@ Game.MainMenu = function(game){
 	this.button_ajustes = null;
 	this.button_tutorial = null;
 	this.image_turn = null;
+	this.text1=null;
+	this.text2=null;
+	this.text3=null;
+	tamano_fuente=60;
 };
 
-
 Game.MainMenu.prototype ={
+    
 
 	
 	create:function(){
+
+        var style = {	font: "menu",
+						fill: "Black",
+						fontSize: "50pt",
+						boundsAlignH: "center",
+						boundsAlignV: "middle",
+					};
+	    
         this.background = this.add.image(0, 0, "background");
         this.background.height = this.game.height;
         this.background.width = this.game.width;
@@ -23,6 +35,27 @@ Game.MainMenu.prototype ={
 		this.button_ajustes.stage='Settings';
 		this.button_tutorial = this.add.button(this.world.centerX, 400, 'button', this.click_button, this, 2, 1, 0);
 		this.button_tutorial.stage='Tutorial';
+		
+		this.text1=this.game.add.text(0, 0, "Batalla",style);
+		this.text2=this.game.add.text(0, 0, "Ajustes",style);
+		this.text3=this.game.add.text(0, 0, "Tutorial",style);
+		this.text1.setTextBounds(0, 200, this.game.world.width,100);
+		this.text2.setTextBounds(0, 300, this.game.world.width,100);
+		this.text3.setTextBounds(0, 400, this.game.world.width,100);
+		
+		
+		/*this.text1.font='Notable-Regular';
+		this.text1.fontSize=tamano_fuente;
+		this.text1.align = 'center';	
+		
+		this.text2.font='Notable-Regular';
+		this.text2.fontSize=tamano_fuente;
+		this.text2.align = 'center';
+		
+		this.text3.font='Notable-Regular';
+		this.text3.fontSize=tamano_fuente;
+		this.text3.align = 'center';*/
+
 	},
 	
 	update:function(){
@@ -72,19 +105,29 @@ Game.MainMenu.prototype ={
 		this.scaleSprite(this.button_inicio, this.world.width, this.world.height / 3, 50, multiplicador_escala);
 		this.button_inicio.x = this.world.centerX- this.button_inicio.width / 2;
 		this.button_inicio.y = (this.world.height*this.porcentaje_logo_juego/100) + (this.world.height*((100-this.porcentaje_logo_juego))/100)/this.num_botones*0;//0 es la posicion
+		//this.text1.x=this.button_inicio.x;
+		//this.text1.y=this.button_inicio.y;
+		this.text1.setTextBounds(0, this.button_inicio.y, this.game.world.width,100);
  
 		this.scaleSprite(this.button_ajustes, this.world.width, this.world.height / 3, 50, multiplicador_escala);
 		this.button_ajustes.x = this.world.centerX- this.button_ajustes.width / 2;
 		this.button_ajustes.y = (this.world.height*this.porcentaje_logo_juego/100) + (this.world.height*((100-this.porcentaje_logo_juego))/100)/this.num_botones*1;//1 es la posicion
+		//this.text2.x=this.button_ajustes.x;
+		//this.text2.y=this.button_ajustes.y;
+		this.text2.setTextBounds(0, this.button_ajustes.y, this.game.world.width,100);
 		
 		this.scaleSprite(this.button_tutorial, this.world.width, this.world.height / 3, 50, multiplicador_escala);
 		this.button_tutorial.x = this.world.centerX - this.button_tutorial.width / 2;
 		this.button_tutorial.y = (this.world.height*this.porcentaje_logo_juego/100) + (this.world.height*((100-this.porcentaje_logo_juego))/100)/this.num_botones*2;//2 es la posicion
+		//this.text3.x=this.button_tutorial.x;
+		//this.text3.y=this.button_tutorial.y;
+		this.text3.setTextBounds(0, this.button_tutorial.y, this.game.world.width,100);
 	},
+
 
 	
 	render:function() {
-		this.game.debug.text(this.game.height+'-W-'+ this.game.width ,40,50,"white");
+		this.game.debug.text(this.text1.font ,40,50,"white");
 	}
 };
 
